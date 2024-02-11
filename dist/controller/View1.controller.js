aae80af43fb1bc2abc1f6c8edfbfa00293b5a040
masdelmon72 <masdelmon@gmail.com>
2024-02-11 14:44:28 +0000

    new file:   .vscode/launch.json 	new file:   dist/Component-dbg.js 	new file:   dist/Component-preload.js 	new file:   dist/Component-preload.js.map 	new file:   dist/Component.js 	new file:   dist/Component.js.map 	new file:   dist/controller/App-dbg.controller.js 	new file:   dist/controller/App.controller.js 	new file:   dist/controller/App.controller.js.map 	new file:   dist/controller/View1-dbg.controller.js 	new file:   dist/controller/View1.controller.js 	new file:   dist/controller/View1.controller.js.map 	new file:   dist/css/style.css 	new file:   dist/i18n/i18n.properties 	new file:   dist/index.html 	new file:   dist/manifest.json 	new file:   dist/model/models-dbg.js 	new file:   dist/model/models.js 	new file:   dist/model/models.js.map 	new file:   dist/sap-ui-cachebuster-info.json 	new file:   dist/sapbtphelloworldui5.zip 	new file:   dist/view/App.view.xml 	new file:   dist/view/View1.view.xml 	new file:   mta.yaml 	new file:   mta_archives/sapbtphelloworldui5_0.0.1.mtar 	new file:   package-lock.json

---
+++ dist/controller/View1.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(e){"use strict";return e.extend("sap.btp.helloworldui5.controller.View1",{onInit:function(){},onPress:function(){MessageBox.alert("You have been alerted!")}})});
+//# sourceMappingURL=View1.controller.js.map

ac5cf83d1b3068c8f17b74a4ae50df4a18fd9d69
masdelmon72 <masdelmon@gmail.com>
2024-02-11 16:18:15 +0000

    modified:   webapp/controller/View1.controller.js

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
+    "sap/m/MessageBox"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("sap.btp.helloworldui5.controller.View1", {
